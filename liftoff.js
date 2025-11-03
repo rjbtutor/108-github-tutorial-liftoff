43ab07d978c31d0da4d2012761bcb9b92bfad171
richardbraytutor <richardbraytutor@gmail.com>
2025-11-03 11:29:22 +0000

    buggy version

---
+++ liftoff.js
@@ -0,0 +1,9 @@
+// Countdown program (buggy version)
+console.log("Starting countdown...");
+
+for (let i = 1; i <= 10; i++) {
+  // BUG: counts up, not down!
+  console.log(i);
+}
+
+console.log("Liftoff!");
